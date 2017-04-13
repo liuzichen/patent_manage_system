995b04bb1634dbc48df126bc0883a17ab4bf73d2
liuzichen <1183301715@qq.com>
2017-04-13 16:52:06 +0800

    comment

---
+++ src/main/java/com/web/project/service/expertService/CommentService.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.web.project.service.expertService;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 子晨
+ *
+ */
+@Service
+public class CommentService {
+
+}
