b1c5ae943d876f36b2b75d4f333baba4f7e946aa
liuzichen <1183301715@qq.com>
2017-04-13 16:09:16 +0800

    专家评审项目

---
+++ src/main/java/com/web/project/dao/CommentDao.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.web.project.dao;
+
+/**
+ * @author 子晨
+ *
+ */
+public class CommentDao {
+
+}
---
+++ src/main/java/com/web/project/model/comment/MakerComWorksComment.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package com.web.project.model.comment;
+
+/**
+ * @author 子晨
+ * 
+ */
+public class MakerComWorksComment {
+	int id;
+	int makercommonworksId;
+	int expertId;
+	String comment;
+	String questions;
+	String commenttime;
+	boolean iscomment;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getMakercommonworksId() {
+		return makercommonworksId;
+	}
+
+	public void setMakercommonworksId(int makercommonworksId) {
+		this.makercommonworksId = makercommonworksId;
+	}
+
+	public int getExpertId() {
+		return expertId;
+	}
+
+	public void setExpertId(int expertId) {
+		this.expertId = expertId;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public String getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(String questions) {
+		this.questions = questions;
+	}
+
+	public String getCommenttime() {
+		return commenttime;
+	}
+
+	public void setCommenttime(String commenttime) {
+		this.commenttime = commenttime;
+	}
+
+	public boolean isIscomment() {
+		return iscomment;
+	}
+
+	public void setIscomment(boolean iscomment) {
+		this.iscomment = iscomment;
+	}
+
+}
